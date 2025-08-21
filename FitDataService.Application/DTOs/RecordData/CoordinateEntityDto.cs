dd6ab49b3d8b308b10ab604f894cf694dad04f05
RubenFern <rubenferval@hotmail.com>
2025-08-21 22:59:22 +0200

    fix

--- FitDataService.Application/DTOs/RecordData/CoordinateEntityDto.cs
+++ FitDataService.Application/DTOs/RecordData/CoordinateEntityDto.cs
@@ -3,6 +3,6 @@ namespace FitDataService.Application.DTOs.RecordData;
 public record CoordinateEntityDto
 {
     public DateTime Timestamp { get; set; }
-    public int? PositionLat { get; set; }
-    public int? PositionLong { get; set; }
+    public double? PositionLat { get; set; }
+    public double? PositionLong { get; set; }
 }
--- FitDataService.Infrastructure/Data/Configurations/Mapping/FitFileDataEntityProfile.cs
+++ FitDataService.Infrastructure/Data/Configurations/Mapping/FitFileDataEntityProfile.cs
@@ -12,19 +12,19 @@ public class FitFileDataEntityProfile : Profile
         CreateMap<ActivityEntityDto, Activity>().ReverseMap();
         CreateMap<FiledIdEntityDto, FileId>().ReverseMap();
         CreateMap<LapEntityDto, Lap>().ReverseMap();
-        CreateMap<RecordEntityDto, Record>()
+        CreateMap<RecordEntityDto, Record>();
+        CreateMap<Record, RecordEntityDto>()
             .ForMember(dest => dest.PositionLat, opt => opt.MapFrom(
                 src => ConvertSemicirclesToDegrees(src.PositionLat)))
             .ForMember(dest => dest.PositionLong, opt => opt.MapFrom(
-                src => ConvertSemicirclesToDegrees(src.PositionLong)))
-            .ReverseMap();
+                src => ConvertSemicirclesToDegrees(src.PositionLong)));
         CreateMap<SessionEntityDto, Session>().ReverseMap();
-        CreateMap<CoordinateEntityDto, Record>()
-            .ForMember(dest => dest.PositionLat, opt => opt.MapFrom(
-                src => ConvertSemicirclesToDegrees(src.PositionLat)))
-            .ForMember(dest => dest.PositionLong, opt => opt.MapFrom(
-                src => ConvertSemicirclesToDegrees(src.PositionLong)))
-            .ReverseMap();
+        CreateMap<CoordinateEntityDto, Record>();
+        CreateMap<Record, CoordinateEntityDto>()
+            .ForMember(dest => dest.PositionLat,
+                opt => opt.MapFrom(src => ConvertSemicirclesToDegrees(src.PositionLat)))
+            .ForMember(dest => dest.PositionLong,
+                opt => opt.MapFrom(src => ConvertSemicirclesToDegrees(src.PositionLong)));
         CreateMap<GraphicsDataEntityDto, Record>().ReverseMap();
     }
 
